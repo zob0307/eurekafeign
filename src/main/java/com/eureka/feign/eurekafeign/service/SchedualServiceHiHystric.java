11840de0a665794573fcb1a6138490a64a740bc7
zhoubi <32322505@qq.com>
2018-01-02 16:54:27 +0800

    a

---
+++ src/main/java/com/eureka/feign/eurekafeign/service/SchedualServiceHiHystric.java
@@ -0,0 +1,11 @@
+package com.eureka.feign.eurekafeign.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
